d86eceddb8492fdf2c37af929f046ca8a3168689
oktarifando <145214026+oktarifando@users.noreply.github.com>
2023-12-18 08:49:08 +0700

    commit

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,10 +1,3 @@
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
-import { NextRequest } from "next/server";
 
-export async function GET(
-  request: Request | NextRequest,
-  { params }: { params: { kindeAuth: string } }
-) {
-  const endpoint = params.kindeAuth;
-  return handleAuth(request, endpoint);
-}
+export const GET = handleAuth();
